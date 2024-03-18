3a61f9a38c1b9ff4851d60d18992be6bc3f0acfe
seonwoo0808 <seonwoo0808@pentag.kr>
2024-03-18 09:19:41 +0900

    :sparkles: Add webview and splash

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from "expo-splash-screen";
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props: {}) {
+    super(props);
+  }
+
+  render() {
+    delay_splash();
+    return (
+      <SafeAreaView style={[styles.container]}>
+      <WebView
+        onLoadEnd={() => SplashScreen.hideAsync()}
+        source={{ 
+          uri: "https://dimi.monster",
+          injectedJavaScript: `let meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); document.getElementsByTagName('head')[0].appendChild(meta);`
+        }}
+        userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+      />
+    </SafeAreaView>
+    );
+  }
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function delay_splash() {
+  await SplashScreen.preventAutoHideAsync();
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
